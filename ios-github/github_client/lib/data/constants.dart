759c79b385f4d1b13f2a3ad477c29d8c20b96716
tathgergely <tath.gergely@gmail.com>
2021-12-21 17:58:18 +0100

    example repository constant added

--- ios-github/github_client/lib/data/constants.dart
+++ ios-github/github_client/lib/data/constants.dart
@@ -1,2 +1,3 @@
 const kGithubToken = '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';
 const kGithubBaseURL = 'https://api.github.com';
+const kAlamofireRepositoryEndpoint = '/repos/Alamofire/Alamofire';
